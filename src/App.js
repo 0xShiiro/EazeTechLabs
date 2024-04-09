0fd92e666c459307ff85e7d4ceb7dc7c43956c87
Codefreakpriyanshu <priyanshusoni0646@gmail.com>
2024-04-09 14:57:54 +0530

    Minor Bug Fixes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import "./App.css";
-import Clockicon from "./components/ClockIcon/Clockicon";
-import Flow from "./components/Flow/Flow";
-import Navbar from "./components/Navbar/Navbar";
-import TextCarousel from "./components/TextCarousel/TextCarousel";
-import Timeline from "./components/Modal/Modal";
 import FlowPage from "./pages/FlowPage";
-import Modal from "./components/Modal/Modal";
 import HomePage from "./pages/HomePage";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 function App() {
